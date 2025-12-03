7424327ae278bb40ca02827e0f15574f2bff8288
Ashwin Pugalia <ashwinpugalia1@gmail.com>
2025-12-03 20:54:59 +0530

    refactor day03

--- src/day03.lua
+++ src/day03.lua
@@ -13,38 +13,38 @@ function solve(banks)
     local joltage, multi_joltage = 0, 0
 
     for _, bank in ipairs(banks) do 
-        local dp = {}
-        function dp.generate_key(index, moves_pending)
+        local cache = {}
+        function cache.generate_key(index, moves_pending)
             return tostring(index) .. ":" .. tostring(moves_pending)
         end
 
-        local function f(index, moves_pending)
+        local function dp(index, moves_pending)
             if moves_pending == 0 then
                 return 0
             end
             if index > #bank then
                 return NINF
             end 
-            local state = dp.generate_key(index, moves_pending)
-            if dp[state] ~= nil then
-                return dp[state]
+            local state = cache.generate_key(index, moves_pending)
+            if cache[state] ~= nil then
+                return cache[state]
             end
-            dp[state] = f(index + 1, moves_pending)
+            cache[state] = dp(index + 1, moves_pending)
             if moves_pending > 0 then
-                local taken = f(index + 1, moves_pending - 1)
+                local taken = dp(index + 1, moves_pending - 1)
                 local current = tonumber(string.sub(bank, index, index))
                 local dig = taken
                 while dig > 0 do
                     current = current * 10
                     dig = dig // 10
                 end
-                dp[state] = math.max(dp[state], current + taken)
+                cache[state] = math.max(cache[state], current + taken)
             end
-            return dp[state]
+            return cache[state]
         end
 
-        joltage = joltage + f(1, 2)
-        multi_joltage = multi_joltage + f(1, 12)
+        joltage = joltage + dp(1, 2)
+        multi_joltage = multi_joltage + dp(1, 12)
     end
     return joltage, multi_joltage
 end
